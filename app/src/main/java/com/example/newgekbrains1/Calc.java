36596151932ee40fa812da4667346be74a9ce409
Max Prus <knigostoriy@gmail.com>
2021-07-16 13:01:47 +0300

    trying calc

---
+++ app/src/main/java/com/example/newgekbrains1/Calc.java
@@ -0,0 +1,5 @@
+package com.example.newgekbrains1;
+
+public class Calc {
+
+}
--- app/src/main/java/com/example/newgekbrains1/MainActivity.java
+++ app/src/main/java/com/example/newgekbrains1/MainActivity.java
@@ -4,15 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView NUMBERS;
+    private int num1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calcul);
+        setContentView(R.layout.activity_main);
+        NUMBERS = findViewById(R.id.numbers1);
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NUMBERS.setText(String.valueOf(2));
+            }
+        });
 
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBottom1();
+            }
+        });
+
+        findViewById(R.id.button12).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NUMBERS.setText(String.valueOf(num1+"+"));
+            }
+        });
+
+        };
+    public void onBottom1(){
+        NUMBERS.setText(String.valueOf(1));
+        num1 = 1;
+    }
+
+    public void getNum1(){
 
     }
-}
+    }
